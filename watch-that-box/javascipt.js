b3af8dc7ab0246cde625055c7da1bfb6df361346
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-08-05 09:50:21 -0700

    updating project

---
+++ watch-that-box/javascipt.js
@@ -0,0 +1,50 @@
+const button2 = document.getElementById('button2');
+
+button2.addEventListener('click', function onClick(event) {
+
+    const box = document.getElementById('box');
+
+    box.style.backgroundColor = 'blue' ;
+
+});
+
+
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px"
+    document.getElementById("box").style.width = "250px"
+  
+
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+
+    document.getElementById("box").style.opacity = 0.5
+
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+
+    document.getElementById("box").style.height = "150px"
+
+    document.getElementById("box").style.width = "150px"
+
+    document.getElementById("box").style.opacity = 1
+
+    document.getElementById("box").style.backgroundColor = "orange"
+
+});
+
+
+
+
+
+
